f1d10e9d17858b132c6a289382ba84c064be09b8
Faiyaz Hasan <faiyaz.hasan1@gmail.com>
2016-09-03 11:20:53 -0400

    Add files via upload

---
+++ Titanic_prediction_script.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Title: Titanic_prediction_script
+Author: Faiyaz Hasan
+File created on Sept. 3, 2016.
+
+In this script, we predict the survivability in the Titanic disaster. This is 
+part of a kaggle competition.
+
+1) We load the test and training data sets from the kaggle website.
+2) Preprocessing of the data:
+    * Remove or impute NA values.
+    * Replace ordinal and nominal features with numerical values.
+3) Exploratory data analysis: What does the data look like? Form intuition! 
+4) Train a machine learning algorithm.    
+5) Test the algorithm.
+6) Try other algorithms and ways to improve them.
+
+"""
+
+#######################################
+# 1. LOAD TRAINING AND TEST DATA SETS #
+#######################################
+
